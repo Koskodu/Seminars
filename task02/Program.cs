266cfb043a60b8f757b3b30b6603cf40d3616edb
Dudyrev <koska.du@mail.ru>
2022-12-15 13:42:29 +0400

    задания 5 и 7

--- task02/Program.cs
+++ task02/Program.cs
@@ -3,23 +3,36 @@
 3 -> Среда
 5 -> Пятница*/
 
-Console.WriteLine("Введите номер дня недели: "); 
+Console.WriteLine("Введите номер дня недели: ");
 int user_input = (Console.ReadLine());
 string weekday = "";
 
-if (user_input == "1") {
+if (user_input == "1")
+{
     weekday = "Понедельник";
-        }else if (user_input == "2"){
+}
+else if (user_input == "2")
+{
     weekday = "вторник";
-        }else if (user_input == "3"){
+}
+else if (user_input == "3")
+{
     weekday = "среда";
-        }else if (user_input == "4"){
+}
+else if (user_input == "4")
+{
     weekday = "четверг";
-        }else if (user_input == "5"){
+}
+else if (user_input == "5")
+{
     weekday = "пятница";
-        }else if (user_input == "6"){
+}
+else if (user_input == "6")
+{
     weekday = "суббота";
-        }else if (user_input == "7"){
+}
+else if (user_input == "7")
+{
     weekday = "Воскресенье";
 }
 else Console.WriteLine("не верный ввод! введите существующий номер дня недели");
