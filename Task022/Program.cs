2cb0eb4d28110d467a02a8639d6c6e44f3bb905b
Dudyrev <koska.du@mail.ru>
2022-12-24 14:55:22 +0400

    Семинары 2, 3

---
+++ Task022/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.*/
+
+void Numb(int n1)
+{
+int counter = 1;
+while(counter <= n1)
+{
+Console.WriteLine($"{counter, 3} {counter * counter, 3}");
+counter++;
+}
+}
+
+System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number < 1)
+{
+System.Console.WriteLine("Введите положительное, целое число");
+number = Convert.ToInt32(Console.ReadLine());
+}
+
+Numb(number);
